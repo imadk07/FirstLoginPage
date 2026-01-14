460a69cd17a1bbe45d4b4f5d56d910279410792c
imadk07 <imadkhan0107@gmail.com>
2026-01-14 19:06:29 +0000

    cors bug fix

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "https://imadsfirstloginpage.netlify.app/",
+      "https://imadsfirstloginpage.netlify.app",
     ],
   })
 );
